5d276c69238ca1b41b41f8385a9a502e32755815
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-04-06 23:11:41 -0500

    Grid Básico

---
+++ basicos_grid/src/components/Grid/Grid.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './../../css/app.css';
+
+const Grid = () => {
+    return (
+        <div className="servicios">
+            <div className="servicio servicio-1">
+                <h2>Servicio 1</h2>
+            </div>
+            <div className="servicio servicio-2">
+                <h2>Servicio 2</h2>
+            </div>
+            <div className="servicio servicio-3">
+                <h2>Servicio 3</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Grid;
